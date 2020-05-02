19e2437eea233df9af7f583b3de2d6577c3ddd03
youngqq <youngqqcn@163.com>
2020-05-02 12:01:15 +0800

    添加模型相关代码_ORM查询操作

--- Python3/Django/StudentsDemo/myApp/models.py
+++ Python3/Django/StudentsDemo/myApp/models.py
@@ -17,20 +17,50 @@ class   Grades(models.Model):
 
 
 
+
+class StudentsManager(models.Manager):
+
+    def get_queryset(self):
+        return super(StudentsManager, self).get_queryset().filter(is_delete=False)
+
+    def create_student(self, name, age, gender, contend, grade, is_delete=False):
+        stu = self.model()
+        stu.sname = name
+        stu.sage = age
+        stu.sgender = gender
+        stu.scontend = contend
+        stu.sgrade = grade
+        stu.is_delete = is_delete
+        return stu
+
+
+
+
 class Students(models.Model):
+
     sname = models.CharField(max_length=20)
     sgender = models.BooleanField(default=True)
     sage = models.IntegerField()
     scontend = models.CharField(max_length=20)
     is_delete = models.BooleanField(default=False)
 
-
     sgrade = models.ForeignKey(Grades, on_delete=models.CASCADE)  #联级删除
 
-    # def __str__(self):
-    #     return self.sname
+
+    stu_all = models.Manager()
+    stu_not_delete = StudentsManager()  #自定义管理器
+
+    def __str__(self):
+        return self.sname
 
 
+    class Meta:
+        db_table = "myApp_students"
+        ordering = ["id"]
 
-
+    @classmethod
+    def create_student(cls, name, age, gender, contend, grade, is_delete=False):
+        stu = cls(sname=name, sage=age, sgender=gender, scontend=contend,
+                  sgrade=grade, is_delete=is_delete)
+        return stu
 
--- Python3/Django/StudentsDemo/myApp/urls.py
+++ Python3/Django/StudentsDemo/myApp/urls.py
@@ -14,5 +14,12 @@ urlpatterns = [
     path(r'grades/all', views.grades_details, name='grades_details'),
     path(r'grades/<int:grade_id>', views.get_grade_students, name='get_grade_students'),
 
-    path(r'students/all', views.students_details, name='students_details')
+    path(r'students/all', views.students_details, name='students_details'),
+    path(r'students/add_default', views.add_default_student, name='add_default_student'),
+    path(r'students/add_default_by_manager', views.add_default_student_use_manager, name='add_default_student_by_manager'),
+    path(r'students/page/<int:page_index>', views.show_students_page, name='show_students_page'),
+    path(r'students/search', views.search_student, name='search_student'),
+    path(r'grades/seach_relate', views.search_relate, name='search_relate'),
+    path(r'grades/girlmorethanboy', views.search_student_by_F, name='search_student_by_F'),
+    path(r'students/older_student', views.search_student_by_Q, name='search_student_by_Q'),
 ]
