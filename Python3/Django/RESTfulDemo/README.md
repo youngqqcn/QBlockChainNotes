7b1a9cc743a69c683960300a1ac2de5b670e4ad9
youngqq <youngqqcn@163.com>
2020-05-06 00:28:17 +0800

    运行 django-rest-framework 的quickstart示例

---
+++ Python3/Django/RESTfulDemo/README.md
@@ -0,0 +1,6 @@
+```
+pip install djangorestframework
+pip install markdown       # Markdown support for the browsable API.
+pip install django-filter  # Filtering support
+
+```
--- Python3/Django/RESTfulDemo/RESTfulDemo/settings.py
+++ Python3/Django/RESTfulDemo/RESTfulDemo/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'rest',
 ]
 
@@ -126,3 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
