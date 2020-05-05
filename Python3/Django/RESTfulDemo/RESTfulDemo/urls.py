1c6917524a96724cd1cf8325115ceb1bd7b90fa6
youngqq <youngqqcn@163.com>
2020-05-06 00:28:17 +0800

    运行 django-rest-framework 的quickstart示例

--- Python3/Django/RESTfulDemo/RESTfulDemo/urls.py
+++ Python3/Django/RESTfulDemo/RESTfulDemo/urls.py
@@ -14,9 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
+from django.urls import include, path
+from rest_framework import routers
+from rest import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+app_name='demorest'
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('rest/', include('rest.urls', namespace='rest'), name='rest'),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
